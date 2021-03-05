fa5e37225e08caf90be95beab60946e964fa5b6e
lorelei47 <948866419@qq.com>
2021-03-05 18:25:56 +0800

    add colorui

--- pages/tabbar/home/home.vue
+++ pages/tabbar/home/home.vue
@@ -1,6 +1,17 @@
 <template>
 	<view>
-		
+		<cu-custom bgImage="https://image.weilanwl.com/color2.0/plugin/sylb2244.jpg" :isBack="true">
+			<block slot="content">索引</block>
+		</cu-custom>
+		<view class="cu-bar bg-white search fixed" :style="[{top:CustomBar + 'px'}]">
+			<view class="search-form round">
+				<text class="cuIcon-search"></text>
+				<input type="text" placeholder="输入搜索的关键词" confirm-type="search"></input>
+			</view>
+			<view class="action">
+				<button class="cu-btn bg-gradual-green shadow-blur round">搜索</button>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,8 +19,12 @@
 	export default {
 		data() {
 			return {
-				
+				StatusBar: this.StatusBar,
+				CustomBar: this.CustomBar,
 			};
+		},
+		methods: {
+			
 		}
 	}
 </script>
