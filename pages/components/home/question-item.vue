046a7256f22933e78d4cea57f6e478dc2d5885c7
lorelei47 <948866419@qq.com>
2021-03-15 23:04:32 +0800

    commits

--- pages/components/home/question-item.vue
+++ pages/components/home/question-item.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="media-item view" v-if="questionItem.questionTitle" @click="click">
-		<view class="view" style="flex-direction: column;">
-			<text></text>
+		<view class="view flex-col">
+			<text class="question-type">{{typeTextIcon(questionItem.questionType)}}</text>
 			<text class="media-title"
 				:class="{'media-title2': questionItem.questionType === 1 || questionItem.questionType === 2}">{{questionItem.questionTitle}}</text>
 		</view>
@@ -9,7 +9,7 @@
 			<view class="media-info flex-row">
 				<text class="info-text"
 					:class="difficultyIcon(questionItem.questionDifficulty)">{{difficultyText(questionItem.questionDifficulty)}}</text>
-				<text class="info-text" v-for="(item, index) in questionItem.questionTag">{{item}}</text>
+				<text class="tag-icon" v-for="(item, index) in questionItem.questionTag">{{item}}</text>
 			</view>
 			<view class="media-info flex-row">
 				<text class="info-text">{{questionItem.uploadDate}}</text>
@@ -29,6 +29,13 @@
 				}
 			}
 		},
+		data() {
+			return {
+				difficultyIconList: ['easyIcon','normalIcon','difficultIcon'],
+				difficultyTextList: ['简单','中等','困难'],
+				typeTextList: ['选','简'],
+			}
+		},
 		methods: {
 			click() {
 				this.$emit('click');
@@ -37,137 +44,107 @@
 				e.stopPropagation();
 				this.$emit('close');
 			},
+			typeTextIcon(e){
+				let val = [0,1].indexOf(e) != -1 ? this.typeTextList[e] : '';
+				return val;
+			},
 			difficultyIcon(e) {
-				let iconClass = ''
-				switch (e) {
-					case 1:
-						iconClass = 'easyIcon';
-						break;
-					case 2:
-						iconClass = 'normalIcon';
-						break;
-					case 3:
-						iconClass = 'difficultIcon';
-						break;
-					default:
-						break;
-				}
-				return iconClass;
+				return e ? this.difficultyIconList[e-1] : '';
 			},
 			difficultyText(e) {
-				let iconText = ''
-				switch (e) {
-					case 1:
-						iconText = '简单';
-						break;
-					case 2:
-						iconText = '中等';
-						break;
-					case 3:
-						iconText = '困难';
-						break;
-					default:
-						break;
-				}
-				return iconText;
+				return e ? this.difficultyTextList[e-1] : '';
 			}
 		}
 	}
 </script>
 
-<style scoped>
-	.view {
-		flex: 1;
-		flex-direction: column;
-	}
-
-	.flex-row {
-		flex-direction: row;
-	}
-
-	.flex-col {
-		flex-direction: column;
-	}
-
-	.list-cell {
-		padding: 0 15px;
-	}
-
-	.uni-list-cell-hover {
-		background-color: #eeeeee;
-	}
-
-	.media-item {
-		position: relative;
-		flex: 1;
-		flex-direction: column;
-		padding: 10px 15px 10px 15px;
-	}
-
-	.media-item-line {
-		position: absolute;
-		left: 15px;
-		right: 15px;
-		bottom: 0;
-		height: 1px;
-		background-color: #ebebeb;
-	}
-
-	.media-image-right {
-		flex-direction: row;
-	}
-
-	.media-image-left {
-		flex-direction: row-reverse;
-	}
-
-	.media-title {
-		flex: 1;
-		lines: 1;
-		text-overflow: ellipsis;
-		overflow: hidden;
-		/* white-space: nowrap; */
-	}
-
-	.media-title {
-		font-size: 17px;
-		font-weight: 600;
-		color: #555555;
-	}
-
-	.media-title2 {
-		flex: 1;
-		margin-top: 5px;
-		line-height: 25px;
-	}
-
-	.media-info {
-		flex-direction: row;
-		align-items: center;
-	}
-
-	.info-text {
-		margin-right: 10px;
-		color: #999999;
-		font-size: 12px;
-	}
-
-	.easyIcon {
-		color: #39b54b;
-	}
-
-	.normalIcon {
-		color: #ffaa00;
-	}
-
-	.difficultIcon {
-		color: #ff3030;
-	}
-
-	.media-foot {
-		margin-top: 12px;
-		flex-direction: row;
-		align-items: center;
-		justify-content: space-between;
+<style lang="scss" scoped>
+	view {
+		.view {
+			flex: 1;
+			flex-direction: column;
+			.flex-row {
+				flex-direction: row;
+			}
+			.flex-col {
+				flex-direction: column;
+				.question-type {
+					margin: 0 5px;
+					padding: 4px 5px;
+					width: 20px;
+					height: 20px;
+					color: #FFFFFF;
+					background-color: #5bad69;
+					font-size: 14px;
+					border-radius: 50%;
+				}
+				.media-title {
+					flex: 1;
+					lines: 1;
+					text-overflow: ellipsis;
+					overflow: hidden;
+					/* white-space: nowrap; */
+				}
+				.media-title {
+					font-size: 17px;
+					font-weight: 600;
+					color: #555555;
+				}
+				.media-title2 {
+					flex: 1;
+					margin-top: 5px;
+					line-height: 25px;
+				}
+			}
+			.media-foot {
+				margin-top: 5px;
+				flex-direction: row;
+				align-items: center;
+				justify-content: space-between;
+				.media-info {
+					flex-direction: row;
+					align-items: center;
+					padding: 3px 0;
+					.info-text {
+						margin-right: 10px;
+						color: #999999;
+						font-size: 12px;
+					}	
+					.tag-icon {
+						margin: 0 5px;
+						padding: 0 3px;
+						font-size: 12px;
+						color: #FFFFFF;
+						background-color: #4aa8ff;
+						border: #35aeff 1px solid;
+						border-radius: 5px;
+						box-shadow: 0.5px 0px 1px #a6caec;
+					}
+					.easyIcon {
+						color: #39b54b;
+					}
+					.normalIcon {
+						color: #ffaa00;
+					}
+					.difficultIcon {
+						color: #ff3030;
+					}
+				}
+			}
+		}
+		.media-item {
+			position: relative;
+			flex: 1;
+			flex-direction: column;
+			padding: 10px 15px 10px 15px;
+			.media-item-line {
+				position: absolute;
+				left: 15px;
+				right: 15px;
+				bottom: 0;
+				height: 1px;
+				background-color: #ebebeb;
+			}
+		}
 	}
 </style>
--- pages/tabbar/home/question.vue
+++ pages/tabbar/home/question.vue
@@ -1,5 +1,6 @@
 <template>
 	<view class="center">
+		<view></view>
 		<view>title</view>
 		<view>
 			<text>写一个左中右布局占满屏幕，其中左右两块是固定宽度200 ，中间自适应宽，要求先加载中间块，请写出结构及样式</text>
